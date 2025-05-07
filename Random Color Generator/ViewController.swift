8837706ed6f5ed73c8fc15c5f2e0e51fe100a144
Luca Park <qlife1146@gmail.com>
2025-05-07 14:24:51 +0900

    Initial Commit

---
+++ Random Color Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Color Generator
+//
+//  Created by Luca Park on 5/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
